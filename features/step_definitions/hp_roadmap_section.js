d3234a8fceef409dd1d079b1706846ee95be93e9
James Lambie <jameslambie@gmail.com>
2018-03-11 22:53:30 +0000

    feat: test

--- features/step_definitions/hp_roadmap_section.js
+++ features/step_definitions/hp_roadmap_section.js
@@ -2,9 +2,8 @@ const expect = require('chai').expect
 
 module.exports = function () {
   this.Then(/^there should be 6 Roadmap update articles visible$/, () => {
-    const articleBoxes = browser.elements('div.grid:first-of-type article')
+    const articleBoxes = browser.elements('div.grid:nth-of-type(1) article')
 
-    console.log(articleBoxes)
     expect(articleBoxes.value.length).to.equal(6)
   })
 }
